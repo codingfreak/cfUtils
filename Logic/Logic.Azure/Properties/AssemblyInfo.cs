c3f88225a84140fdc41993a1a6cf975b34743d74
codingfreak <sprinter@codingfreaks.de>
2016-02-10 23:36:08 +0100

    New Release

--- Logic/Logic.Azure/Properties/AssemblyInfo.cs
+++ Logic/Logic.Azure/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfAzureUtils")]
 [assembly: AssemblyDescription("[codingfreaks] Azure utility library")]
-[assembly: AssemblyVersion("1.0.11.20")]
-[assembly: AssemblyFileVersion("1.0.11.20")]
+[assembly: AssemblyVersion("1.0.12.21")]
+[assembly: AssemblyFileVersion("1.0.12.21")]
 [assembly: ComVisible(false)]
