df532a9de5f1c3c029dde002e31ba2d37e88f838
Alexander Schmidt <sprinter@codingfreaks.de>
2016-07-30 18:06:15 +0200

    Added dependencies.

--- Logic/Logic.WebMvc/Properties/AssemblyInfo.cs
+++ Logic/Logic.WebMvc/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfWebMvcUtils")]
 [assembly: AssemblyDescription("[codingfreaks] web utility library for MVC")]
-[assembly: AssemblyVersion("1.0.7.11")]
-[assembly: AssemblyFileVersion("1.0.7.11")]
+[assembly: AssemblyVersion("1.0.8.12")]
+[assembly: AssemblyFileVersion("1.0.8.12")]
 [assembly: ComVisible(false)]
