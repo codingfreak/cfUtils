30e2449f5ff6811064608501540402d448884d3f
Alexander Schmidt <sprinter@codingfreaks.de>
2016-08-09 21:48:43 +0200

    Re-Inserted old strategy

---
+++ Logic/Logic.Utils/Misc/SqlAzureDatabaseConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace codingfreaks.cfUtils.Logic.Utils.Misc
+{
+    using System.Data.Entity;
+    using System.Data.Entity.SqlServer;
+
+    using Base.Utilities;
+
+    /// <summary>
+    /// A reusable database configuration for SQL Azure
+    /// </summary>
+    public class SqlAzureDatabaseConfiguration : DbConfiguration
+    {
+        #region constructors and destructors
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SqlAzureDatabaseConfiguration()
+        {
+            TraceUtil.WriteTraceDebug("Using custom database configuration.");
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            SetDatabaseLogFormatter((ctx, logAction) => new OneLineDatabaseLogFormatter(ctx, logAction));
+        }
+
+        #endregion
+    }
+}
--- Logic/Logic.Utils/Properties/AssemblyInfo.cs
+++ Logic/Logic.Utils/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfExtendedUtils")]
 [assembly: AssemblyDescription("[codingfreaks] extended utility library")]
-[assembly: AssemblyVersion("1.0.19.26")]
-[assembly: AssemblyFileVersion("1.0.19.26")]
+[assembly: AssemblyVersion("1.0.20.27")]
+[assembly: AssemblyFileVersion("1.0.20.27")]
 [assembly: ComVisible(false)]
