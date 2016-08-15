2677b4734bb76777aac9595d749ed3ec08a5b801
Alexander Schmidt <sprinter@codingfreaks.de>
2016-08-15 12:29:32 +0200

    New version of Logic.Utils.

--- Logic/Logic.Utils/Misc/SuspendableSqlAzureExecutionStrategy.cs
+++ Logic/Logic.Utils/Misc/SuspendableSqlAzureExecutionStrategy.cs
@@ -10,8 +10,12 @@
     using System.Threading.Tasks;
 
     /// <summary>
-    /// http://stackoverflow.com/questions/31899382/how-to-use-sqlazureexecutionstrategy-and-nolock/32102380#32102380
+    /// An execution strategy for scenarios in which transactional handling is important.
     /// </summary>
+    /// <remarks>
+    /// This code is mainly taken from suggestions at
+    /// http://stackoverflow.com/questions/31899382/how-to-use-sqlazureexecutionstrategy-and-nolock/32102380#32102380
+    /// </remarks>
     public class SuspendableSqlAzureExecutionStrategy : IDbExecutionStrategy
     {
         #region member vars
@@ -22,6 +26,9 @@
 
         #region constructors and destructors
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public SuspendableSqlAzureExecutionStrategy()
         {
             _azureExecutionStrategy = new SqlAzureExecutionStrategy();
@@ -31,6 +38,10 @@
 
         #region explicit interfaces
 
+        /// <summary>
+        /// Executes the specified operation.
+        /// </summary>
+        /// <param name="operation">A delegate representing an executable operation that doesn't return any results.</param>
         public virtual void Execute(Action operation)
         {
             if (!RetriesOnFailure)
@@ -50,6 +61,15 @@
             }
         }
 
+        /// <summary>
+        /// Executes the specified operation and returns the result.
+        /// </summary>
+        /// <typeparam name="TResult"> The return type of <paramref name="operation" />. </typeparam>
+        /// <param name="operation">
+        /// A delegate representing an executable operation that returns the result of type
+        /// <typeparamref name="TResult" />.
+        /// </param>
+        /// <returns>The result from the operation.</returns>
         public virtual TResult Execute<TResult>(Func<TResult> operation)
         {
             if (!RetriesOnFailure)
@@ -68,6 +88,19 @@
             }
         }
 
+        /// <summary>
+        /// Executes the specified asynchronous operation.
+        /// </summary>
+        /// <param name="operation">A function that returns a started task.</param>
+        /// <param name="cancellationToken">
+        /// A cancellation token used to cancel the retry operation, but not operations that are already in flight
+        /// or that already completed successfully.
+        /// </param>
+        /// <returns>
+        /// A task that will run to completion if the original task completes successfully (either the
+        /// first time or after retrying transient failures). If the task fails with a non-transient error or
+        /// the retry limit is reached, the returned task will become faulted and the exception must be observed.
+        /// </returns>
         public virtual async Task ExecuteAsync(Func<Task> operation, CancellationToken cancellationToken)
         {
             if (!RetriesOnFailure)
@@ -87,13 +120,29 @@
             }
         }
 
+        /// <summary>
+        /// Executes the specified asynchronous operation and returns the result.
+        /// </summary>
+        /// <typeparam name="TResult">
+        /// The result type of the <see cref="T:System.Threading.Tasks.Task`1" /> returned by
+        /// <paramref name="operation" />.
+        /// </typeparam>
+        /// <param name="operation">A function that returns a started task of type <typeparamref name="TResult" />. </param>
+        /// <param name="cancellationToken">
+        /// A cancellation token used to cancel the retry operation, but not operations that are already in flight
+        /// or that already completed successfully.
+        /// </param>
+        /// <returns>
+        /// A task that will run to completion if the original task completes successfully (either the
+        /// first time or after retrying transient failures). If the task fails with a non-transient error or
+        /// the retry limit is reached, the returned task will become faulted and the exception must be observed.
+        /// </returns>
         public virtual async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> operation, CancellationToken cancellationToken)
         {
             if (!RetriesOnFailure)
             {
                 return await operation();
             }
-
             try
             {
                 Suspend = true;
@@ -105,14 +154,21 @@
             }
         }
 
-        public bool RetriesOnFailure
-        {
-            get
-            {
-                return !Suspend;
-            }
-        }
+        /// <summary>
+        /// Indicates whether this <see cref="T:System.Data.Entity.Infrastructure.IDbExecutionStrategy" /> might retry the
+        /// execution after a failure.
+        /// </summary>
+        public bool RetriesOnFailure => !Suspend;
 
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Defines the retry behavior of this strategy.
+        /// </summary>
+        /// <param name="exception">Any exception that occured on the context.</param>
+        /// <returns><c>true</c> if a retry should by performed otherwise <c>false</c>.</returns>
         protected virtual bool ShouldRetryOn(Exception exception)
         {
             var shouldRetry = false;
@@ -136,6 +192,9 @@
 
         #region properties
 
+        /// <summary>
+        /// Gets or sets the strategy name from/in the current call context.
+        /// </summary>
         private static bool Suspend
         {
             get
--- Logic/Logic.Utils/Properties/AssemblyInfo.cs
+++ Logic/Logic.Utils/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfExtendedUtils")]
 [assembly: AssemblyDescription("[codingfreaks] extended utility library")]
-[assembly: AssemblyVersion("1.0.20.27")]
-[assembly: AssemblyFileVersion("1.0.20.27")]
+[assembly: AssemblyVersion("1.0.21.28")]
+[assembly: AssemblyFileVersion("1.0.21.28")]
 [assembly: ComVisible(false)]
