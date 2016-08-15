edffd1aa1bffcd9b1c0b088e029a2345b8f87869
Alexander Schmidt <sprinter@codingfreaks.de>
2016-08-15 15:47:07 +0200

    New release

--- Logic/Logic.Web/Properties/AssemblyInfo.cs
+++ Logic/Logic.Web/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfWebUtils")]
 [assembly: AssemblyDescription("[codingfreaks] web utility library")]
-[assembly: AssemblyVersion("1.0.7.19")]
-[assembly: AssemblyFileVersion("1.0.7.19")]
+[assembly: AssemblyVersion("1.0.8.20")]
+[assembly: AssemblyFileVersion("1.0.8.20")]
 [assembly: ComVisible(false)]
