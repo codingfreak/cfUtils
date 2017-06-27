007f88c02dd50ff02c911a716f46b2f08a87cf7a
Alexander Schmidt <sprinter@codingfreaks.de>
2017-06-27 17:31:15 +0200

    Updated references for test-project and new version of web logic.

--- Logic/Logic.Web/Properties/AssemblyInfo.cs
+++ Logic/Logic.Web/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfWebUtils")]
 [assembly: AssemblyDescription("[codingfreaks] web utility library")]
-[assembly: AssemblyVersion("1.0.10.23")]
-[assembly: AssemblyFileVersion("1.0.10.23")]
+[assembly: AssemblyVersion("1.0.11.24")]
+[assembly: AssemblyFileVersion("1.0.11.24")]
 [assembly: ComVisible(false)]
