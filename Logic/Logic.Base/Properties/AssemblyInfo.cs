6c953aa4aabdc264db25cfeb87292c9532f361c9
Alexander Schmidt <sprinter@codingfreaks.de>
2017-06-27 17:19:41 +0200

    Updated base utils package.

--- Logic/Logic.Base/Properties/AssemblyInfo.cs
+++ Logic/Logic.Base/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("cfBaseUtils")]
 [assembly: AssemblyDescription("[codingfreaks] base utility library")]
-[assembly: AssemblyVersion("1.0.18.33")]
-[assembly: AssemblyFileVersion("1.0.18.33")]
+[assembly: AssemblyVersion("1.0.19.34")]
+[assembly: AssemblyFileVersion("1.0.19.34")]
