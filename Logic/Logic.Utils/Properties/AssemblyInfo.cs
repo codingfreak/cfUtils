ce6d4a16436528239b99abdf68267c0b2c78d1e2
Alexander Schmidt <sprinter@codingfreaks.de>
2017-07-05 13:08:42 +0200

    Package update for extended utils

--- Logic/Logic.Utils/Properties/AssemblyInfo.cs
+++ Logic/Logic.Utils/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfExtendedUtils")]
 [assembly: AssemblyDescription("[codingfreaks] extended utility library")]
-[assembly: AssemblyVersion("1.0.24.35")]
-[assembly: AssemblyFileVersion("1.0.24.35")]
+[assembly: AssemblyVersion("1.0.25.36")]
+[assembly: AssemblyFileVersion("1.0.25.36")]
 [assembly: ComVisible(false)]
