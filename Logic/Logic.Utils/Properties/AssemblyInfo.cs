38c6f739663e850bd03a748ab158da2b95fb2c87
Alexander Schmidt <sprinter@codingfreaks.de>
2017-07-06 12:49:27 +0200

    Updated references

--- Logic/Logic.Utils/Properties/AssemblyInfo.cs
+++ Logic/Logic.Utils/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfExtendedUtils")]
 [assembly: AssemblyDescription("[codingfreaks] extended utility library")]
-[assembly: AssemblyVersion("1.0.26.37")]
-[assembly: AssemblyFileVersion("1.0.26.37")]
+[assembly: AssemblyVersion("1.0.27.38")]
+[assembly: AssemblyFileVersion("1.0.27.38")]
 [assembly: ComVisible(false)]
