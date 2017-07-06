888267fff18ddb704b90e8f25e6b25dcfccb0c7c
Alexander Schmidt <sprinter@codingfreaks.de>
2017-07-06 12:47:49 +0200

    Updated reference

--- Logic/Logic.Base/Properties/AssemblyInfo.cs
+++ Logic/Logic.Base/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("cfBaseUtils")]
 [assembly: AssemblyDescription("[codingfreaks] base utility library")]
-[assembly: AssemblyVersion("1.0.23.38")]
-[assembly: AssemblyFileVersion("1.0.23.38")]
+[assembly: AssemblyVersion("1.0.24.39")]
+[assembly: AssemblyFileVersion("1.0.24.39")]
