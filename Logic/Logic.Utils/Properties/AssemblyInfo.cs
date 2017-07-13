dafcf1c94d257f6d0bf4527d5c9cf8d08b9c6e0e
Alexander Schmidt <sprinter@codingfreaks.de>
2017-07-13 10:18:28 +0200

    Package updates

--- Logic/Logic.Utils/Properties/AssemblyInfo.cs
+++ Logic/Logic.Utils/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfExtendedUtils")]
 [assembly: AssemblyDescription("[codingfreaks] extended utility library")]
-[assembly: AssemblyVersion("1.0.27.38")]
-[assembly: AssemblyFileVersion("1.0.27.38")]
+[assembly: AssemblyVersion("1.0.28.39")]
+[assembly: AssemblyFileVersion("1.0.28.39")]
 [assembly: ComVisible(false)]
