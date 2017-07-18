29855e78e60b80e301575d78f82e0063eb31a441
Alexander Schmidt <sprinter@codingfreaks.de>
2017-07-18 13:29:37 +0200

    Added Json handling to MVC

--- Logic/Logic.WebMvc/Properties/AssemblyInfo.cs
+++ Logic/Logic.WebMvc/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfWebMvcUtils")]
 [assembly: AssemblyDescription("[codingfreaks] web utility library for MVC")]
-[assembly: AssemblyVersion("1.0.9.15")]
-[assembly: AssemblyFileVersion("1.0.9.15")]
+[assembly: AssemblyVersion("1.0.10.16")]
+[assembly: AssemblyFileVersion("1.0.10.16")]
 [assembly: ComVisible(false)]
