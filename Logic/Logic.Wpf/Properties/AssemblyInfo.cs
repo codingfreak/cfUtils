a9607bd8ba27a528bed91dc8a2c55e3a0f45be07
Alexander Schmidt <sprinter@codingfreaks.de>
2017-08-28 18:42:15 +0200

    Nuget update

--- Logic/Logic.Wpf/Properties/AssemblyInfo.cs
+++ Logic/Logic.Wpf/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("cfWpfUtils")]
 [assembly: AssemblyDescription("[codingfreaks] WPF utility library")]
-[assembly: AssemblyVersion("1.0.22.29")]
-[assembly: AssemblyFileVersion("1.0.22.29")]
+[assembly: AssemblyVersion("1.0.24.31")]
+[assembly: AssemblyFileVersion("1.0.24.31")]
 [assembly: ComVisible(false)]
