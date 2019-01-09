4488664031a70315df460b2be049773dff646899
Alexander Schmidt <alexander.schmidt@devdeer.com>
2019-01-09 23:54:00 +0100

    CheckUtil tests implemented

--- Logic/Logic.Core/Utilities/CheckUtil.cs
+++ Logic/Logic.Core/Utilities/CheckUtil.cs
@@ -1,7 +1,6 @@
 ﻿namespace codingfreaks.cfUtils.Logic.Core.Utilities
 {
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Linq.Expressions;
@@ -12,7 +11,7 @@
     public static class CheckUtil
     {
         #region methods
-        
+
         /// <summary>
         /// Throws an <see cref="ArgumentException" /> containing the name of the parameter in the <paramref name="expression" />
         /// if it is not
@@ -21,12 +20,14 @@
         /// <example>
         /// void Foo(object someData)
         /// {
-        /// CheckUtil.ThrowIfInvalidEnum[MyEnumType](() => myEnumValue);
+        ///     CheckUtil.ThrowIfInvalidEnum[MyEnumType](() => myEnumValue);
         /// }
         /// </example>
         /// <typeparam name="TEnum">The type of the enumeration in which the <paramref name="expression" /> must be contained.</typeparam>
         /// <param name="expression">The expression containing the enum-variable.</param>
-        /// <param name="callbackOnException">An optional callback method that will be called right before the expection throwing.</param>
+        /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Is thrown if the <paramref name="expression"/> retrieves an enum value not defined in <typeparamref name="TEnum"/>.</exception>
         public static void ThrowIfInvalidEnum<TEnum>(Expression<Func<TEnum>> expression, Action<Exception> callbackOnException = null)
         {
             if (expression == null)
@@ -38,8 +39,8 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentException("Provided value is not part of the enumeration", expressionBody.Member.Name);
+            var expressionBody = (ConstantExpression)expression.Body;
+            var ex = new ArgumentException($"Provided value [{expressionBody}] is not part of the enumeration");
             callbackOnException?.Invoke(ex);
             throw ex;
         }
@@ -57,6 +58,8 @@
         /// <typeparam name="T">The type of the parameter which will be passed.</typeparam>
         /// <param name="expression">The expression containing the variable.</param>
         /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="NullReferenceException">Is thrown if the <paramref name="expression"/> retrieves <c>null</c>.</exception>
         [SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "1", Justification = "This CA-Warning is simply wrong.")]
         public static void ThrowIfNull<T>(Expression<Func<T>> expression, Action<Exception> callbackOnException = null)
             where T : class
@@ -69,14 +72,19 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentNullException(expressionBody.Member.Name);
+            var message = string.Empty;
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                message = memberExpression.Member.Name;
+            }            
+            var ex = new NullReferenceException(message);
             callbackOnException?.Invoke(ex);
             throw ex;
         }
 
         /// <summary>
-        /// Throws an <see cref="ArgumentNullException" /> containing the name of the parameter in the <paramref name="expression" />
+        /// Throws an <see cref="ArgumentNullException" /> containing the name of the parameter in the
+        /// <paramref name="expression" />
         /// if contains null or is equal to <see cref="string.Empty" />.
         /// </summary>
         /// <example>
@@ -87,6 +95,8 @@
         /// </example>
         /// <param name="expression">The expression containing the variable.</param>
         /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Is thrown if the <paramref name="expression"/> returns an empty string or <c>null</c>.</exception>
         public static void ThrowIfNullOrEmpty(Expression<Func<string>> expression, Action<Exception> callbackOnException = null)
         {
             if (expression == null)
@@ -97,8 +107,15 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentNullException(expressionBody.Member.Name);
+            ArgumentException ex;
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                ex = new ArgumentException("Value can't be null or empty.", memberExpression.Member.Name);
+            }
+            else
+            {
+                ex = new ArgumentException("Value can't be null or empty.");
+            }
             callbackOnException?.Invoke(ex);
             throw ex;
         }
@@ -115,6 +132,8 @@
         /// </example>
         /// <param name="expression">The expression containing the variable.</param>
         /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Is thrown if the <paramref name="expression"/> returns an empty string, <c>null</c> or just a white space.</exception>
         public static void ThrowIfNullOrWhitespace(Expression<Func<string>> expression, Action<Exception> callbackOnException = null)
         {
             if (expression == null)
@@ -125,8 +144,15 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentException("Value can't be null or white space.", expressionBody.Member.Name);
+            ArgumentException ex;
+            if (expression.Body is MemberExpression memberExpression)
+            {                
+                ex = new ArgumentException("Value can't be null or white space.", memberExpression.Member.Name);
+            }
+            else
+            {
+                ex = new ArgumentException("Value can't be null or white space.");
+            }            
             callbackOnException?.Invoke(ex);
             throw ex;
         }
@@ -143,6 +169,8 @@
         /// </example>
         /// <param name="expression">The expression containing the variable.</param>
         /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Is thrown if the <paramref name="expression"/> returns a number smaller or equal to 0.</exception>
         public static void ThrowIfZeroOrNegative(Expression<Func<long>> expression, Action<Exception> callbackOnException = null)
         {
             if (expression == null)
@@ -153,8 +181,15 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentException("Value can't be zero or negative.", expressionBody.Member.Name);
+            ArgumentException ex;
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                ex = new ArgumentException("Value can't be zero or negative.", memberExpression.Member.Name);
+            }
+            else
+            {
+                ex = new ArgumentException("Value can't be zero or negative.");
+            }            
             callbackOnException?.Invoke(ex);
             throw ex;
         }
@@ -171,6 +206,8 @@
         /// </example>
         /// <param name="expression">The expression containing the variable.</param>
         /// <param name="callbackOnException">An optional callback method that will be called right before the exception throwing.</param>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="expression"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Is thrown if the <paramref name="expression"/> returns a number smaller or equal to 0.</exception>
         public static void ThrowIfZeroOrNegative(Expression<Func<int>> expression, Action<Exception> callbackOnException = null)
         {
             if (expression == null)
@@ -181,8 +218,15 @@
             {
                 return;
             }
-            var expressionBody = (MemberExpression)expression.Body;
-            var ex = new ArgumentException("Value can't be zero or negative.", expressionBody.Member.Name);
+            ArgumentException ex;
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                ex = new ArgumentException("Value can't be zero or negative.", memberExpression.Member.Name);
+            }
+            else
+            {
+                ex = new ArgumentException("Value can't be zero or negative.");
+            }
             callbackOnException?.Invoke(ex);
             throw ex;
         }
---
+++ Tests/Tests.Logic.Core/CheckUtilTests.cs
@@ -0,0 +1,237 @@
+﻿namespace codingfreaks.cfUtils.Tests.Logic.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection.Metadata.Ecma335;
+
+    using cfUtils.Logic.Core.Enumerations;
+    using cfUtils.Logic.Core.Utilities;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NUnit.Framework;
+    using NUnit.Framework.Constraints;
+    using NAssert = NUnit.Framework.Assert;
+
+    /// <summary>
+    /// Contains unit tests for <see cref="CheckUtil" />.
+    /// </summary>
+    [TestClass]
+    public class CheckUtilTests
+    {
+        #region methods
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfInvalidEnum{TEnum}" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfInvalidEnumTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<PortStateEnum>> expression, bool shouldRaceNull, bool shouldRaceArgument)>
+            {
+                (null, true, false),
+                (() => (PortStateEnum)10, false, true),
+                (() => PortStateEnum.Open, false, false),
+                (() => (PortStateEnum)1, false, false)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfInvalidEnum(value.expression), "ArgumentNullException expected but not thrown.");
+                }                                                    
+                if (value.shouldRaceArgument)
+                {
+                    NAssert.Throws<ArgumentException>(() => CheckUtil.ThrowIfInvalidEnum(value.expression), "ArgumentException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceArgument)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfInvalidEnum(value.expression)), "Unexpected exception.");
+                }                
+            }            
+        }
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfNull{T}" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfNullTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<object>> expression, bool shouldRaceNull, bool shouldRaceRefNull)>
+            {
+                (null, true, false),
+                (() => null, false, true),
+                (() => "Hello", false, false),
+                (() => DateTime.Now, false, false),
+                (() => default(DateTime?), false, true),
+                (() => default(int?), false, true)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfNull(value.expression), "ArgumentNullException expected but not thrown.");
+                }
+                if (value.shouldRaceRefNull)
+                {
+                    NAssert.Throws<NullReferenceException>(() => CheckUtil.ThrowIfNull(value.expression), "NullReferenceException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceRefNull)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfNull(value.expression)), "Unexpected exception.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfNullOrEmpty" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfNullOrEmptyTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<string>> expression, bool shouldRaceNull, bool shouldRaceArg)>
+            {
+                (null, true, false),
+                (() => string.Empty, false, true),
+                (() => null, false, true),
+                (() => "Hello World!", false, false),
+                (() => "1", false, false),
+                (() => " ", false, false)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfNullOrEmpty(value.expression), "ArgumentNullException expected but not thrown.");
+                }
+                if (value.shouldRaceArg)
+                {
+                    NAssert.Throws<ArgumentException>(() => CheckUtil.ThrowIfNullOrEmpty(value.expression), "ArgumentException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceArg)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfNullOrEmpty(value.expression)), "Unexpected exception.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfNullOrWhitespace" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfNullOrWhitespaceTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<string>> expression, bool shouldRaceNull, bool shouldRaceArg)>
+            {
+                (null, true, false),
+                (() => string.Empty, false, true),
+                (() => null, false, true),
+                (() => "Hello World!", false, false),
+                (() => "1", false, false),
+                (() => " ", false, true),
+                (() => "  ", false, true),
+                (() => new string(' ', 1000), false, true)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfNullOrWhitespace(value.expression), "ArgumentNullException expected but not thrown.");
+                }
+                if (value.shouldRaceArg)
+                {
+                    NAssert.Throws<ArgumentException>(() => CheckUtil.ThrowIfNullOrWhitespace(value.expression), "ArgumentException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceArg)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfNullOrWhitespace(value.expression)), "Unexpected exception.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfZeroOrNegative(Expression{System.Func{long}},Action{System.Exception})" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfZeroOrNegativeLongTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<long>> expression, bool shouldRaceNull, bool shouldRaceArg)>
+            {
+                (null, true, false)   ,
+                (() => 0L, false, true),
+                (() => 1L, false, false),
+                (() => long.MinValue, false, true),
+                (() => long.MaxValue, false, false),
+                (() => (long)1.5, false, false),
+                (() => (long).5, false, true),
+                (() => (long).99, false, true)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfZeroOrNegative(value.expression), "ArgumentNullException expected but not thrown.");
+                }
+                if (value.shouldRaceArg)
+                {
+                    NAssert.Throws<ArgumentException>(() => CheckUtil.ThrowIfZeroOrNegative(value.expression), "ArgumentException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceArg)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfZeroOrNegative(value.expression)), "Unexpected exception.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests the correct functionality of <see cref="CheckUtil.ThrowIfZeroOrNegative(Expression{System.Func{int}},Action{System.Exception})" />.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfZeroOrNegativeIntTest()
+        {
+            // arrange
+            var values = new List<(Expression<Func<int>> expression, bool shouldRaceNull, bool shouldRaceArg)>
+            {
+                (null, true, false)   ,
+                (() => 0, false, true),
+                (() => 1, false, false),
+                (() => int.MinValue, false, true),
+                (() => int.MaxValue, false, false),
+                (() => (int)1.5, false, false),
+                (() => (int).5, false, true),
+                (() => (int).99, false, true)
+            };
+            // act & assert
+            foreach (var value in values)
+            {
+                if (value.shouldRaceNull)
+                {
+                    NAssert.Throws<ArgumentNullException>(() => CheckUtil.ThrowIfZeroOrNegative(value.expression), "ArgumentNullException expected but not thrown.");
+                }
+                if (value.shouldRaceArg)
+                {
+                    NAssert.Throws<ArgumentException>(() => CheckUtil.ThrowIfZeroOrNegative(value.expression), "ArgumentException expected but not thrown.");
+                }
+                if (!value.shouldRaceNull && !value.shouldRaceArg)
+                {
+                    NAssert.DoesNotThrow((() => CheckUtil.ThrowIfZeroOrNegative(value.expression)), "Unexpected exception.");
+                }
+            }
+        }
+
+        #endregion
+    }
+}
